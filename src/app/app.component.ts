6a7db017b077df3e9e0738f050057a00eaed8555
Andres Rodriguez <andresarodriguezr89@gmail.com>
2023-06-06 12:53:03 -0500

    primer proyecto e comerce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Expansion } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { ActivationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'tuto';
+  age = 33;
+  img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy2WjdhMiqnUiCACM2rVHhgMbASutf8639bw&usqp=CAU';
+  img2= 'https://skinsdefortnite.com/wp-content/uploads/2022/04/Skin-Jill-Valentine.png';
+  btnDisabled = true;
+  
+  person ={
+    name:'andres',
+    age: 33,
+    avatar:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy2WjdhMiqnUiCACM2rVHhgMbASutf8639bw&usqp=CAU',
+
+  }
+   toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+   }
+
+   increseage(){
+    this.person.age += 1;
+   }
+
+    decreaseage(){
+      this.person.age -= 1;
+    } 
+
+    onscroll(event: Event){
+      const element = event.target as HTMLElement;
+      console.log(element.scrollTop);
+
+    }
+
+    changeName(event: Event){
+      const element = event.target as HTMLInputElement;
+      this.person.name = element.value;
+    }
+    
+  
 }
+
+
---
+++ src/app/recap.ts
@@ -0,0 +1,12 @@
+const username: string= 'tuto';
+const sum = (a: number, b: number) => { 
+    return a + b;
+}
+sum(1,2)
+
+class person {
+   
+    constructor(public age: number,public lastName: string){}
+}
+
+const tuto = new person(15, 'Rodriguez')
